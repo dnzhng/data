5a834e13ce0a66a7c690e9b3cc172d1cc8b28f2f
Dan Zhang <dyz2@duke.edu>
2015-02-14 01:23:44 -0500

    starter code for publication word analysis

---
+++ javascripts/publications.js
@@ -0,0 +1,57 @@
+loadJson(d3.select("#dataSelect").property("value"));
+d3.select("#dataSelect").on("change", function() { loadJson(this.value) });
+d3.select("#submit").on("click", function() { d3.select("svg").remove(); visualize(JSON.parse(d3.select("#textarea").property("value"))); });
+
+function loadJson(dataSet) {
+  d3.json("/datasets/" + dataSet + ".json", function(error, json) {
+    if (error) return console.warn(error);
+    d3.select("svg").remove();
+    visualize(json);
+  });
+}
+
+function visualize (data) {
+
+  var wordCounts = {};  
+
+  data.publications.map(function(x) { incrementWordCount(x.label, wordCounts); });
+
+  console.log(wordCounts);
+  var margin = {top: 20, right: 20, bottom: 80, left: 40},
+  width = 1200 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom
+  aspect = 500/1200;
+
+  var svg = d3.select("#publications").append("svg")
+  .attr("preserveAspectRatio", "xMidYMid")
+  .attr("viewBox", "0,0,1200,500")
+  .attr("class", "class")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", (width + margin.top + margin.bottom)*aspect)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+  resize();
+  d3.select(window).on('resize.class', resize);
+
+  function incrementWordCount(s, obj) {
+    var title = s.split(/\W+/);
+    title.map(function(word) {
+      var w = word.toLowerCase();
+      if (obj[w]) {
+        obj[w]++;
+      } else {
+        obj[w] = 1;
+      }
+    });
+  }
+
+  function resize() {
+   width = parseInt(d3.select("#publications").style("width"), 10);
+   width = width - margin.left - margin.right;
+   d3.select("svg.class").attr("width", width + margin.left + margin.right);
+   d3.select("svg.class").attr("height", (width + margin.top + margin.bottom) * aspect);
+ }
+}
